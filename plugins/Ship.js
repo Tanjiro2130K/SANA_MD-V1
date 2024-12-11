6651f7669c7c3489c48339d004d012492d479814
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-12-11 18:50:02 +0530

    Update Ship.js

--- plugins/Ship.js
+++ plugins/Ship.js
@@ -5,7 +5,7 @@ function handler(m, { groupMetadata }) {
     let b
     do b = ps.getRandom()
     while (b === a)
-    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations 💖🍻`, null, {
+    m.reply(`${toM(a)} 🫶🏻 ${toM(b)}\nCongratulations you two are now couples ✨🦪`, null, {
         mentions: [a, b]
     })
 }
